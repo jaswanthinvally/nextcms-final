e96ea059c0be388303d0d2059970809b3277f25a
jaswanthinvally <jaswanthvenkat001@gmail.com>
2025-01-22 20:56:19 +0530

    first commit of the final project

---
+++ app/welcome/admin/page.tsx
@@ -0,0 +1,8 @@
+export function AdminPage () {
+    return (
+        <div>
+            welcome to the admin page
+        </div>
+    )
+
+}
---
+++ app/welcome/page.tsx
@@ -0,0 +1,12 @@
+export default function Welcome () {
+    return (
+        <div>
+            <div>
+                Welcome to the NextGen CMS  
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
